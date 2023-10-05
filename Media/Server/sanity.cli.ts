bdf6b70479acaca5757d20f812663b6aa68c0ef3
unknown <chahrazedould15@gmail.com>
2023-10-06 01:06:01 +0200

    feature(Server): project init + adding schemas

---
+++ Media/Server/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '6q2yh8uj',
+    dataset: 'production'
+  }
+})
---
+++ Media/Server/schemas/index.ts
@@ -0,0 +1,6 @@
+import userSchema from './user';
+import pinSchema from './pin';
+import commentSchema from './comment';
+import saveSchema from './save';
+import postedBySchema from './postedBy';
+export const schemaTypes = [userSchema,pinSchema,commentSchema,saveSchema,postedBySchema];
